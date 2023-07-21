3e2638c9c4ed8053c97ef0b1b79c1148433ebfda
takahiro.tominaga <tt.300607@gmail.com>
2023-07-22 01:25:11 +0900

    finish quiz app

--- VueQuizApp/src/views/QuizView.vue
+++ VueQuizApp/src/views/QuizView.vue
@@ -1,13 +1,57 @@
 <script setup>
 import Question from "../components/Question.vue";
 import QuestionHeader from "../components/QuizHeader.vue";
+import Result from "../components/Result.vue";
+import { useRoute } from "vue-router";
+import { ref, computed } from "vue";
+import quizes from "../data/quizes.json";
+
+const router = useRoute();
+const id = parseInt(router.params.id);
+const quiz = quizes.find((quiz) => quiz.id === id);
+const currentQuestionIndex = ref(0);
+const numberOfCorrectAnswers = ref(0);
+const showResults = ref(false);
+
+// const questionStatus = `${currentQuestionIndex.value + 1} / ${quiz.questions.length}`;
+// watch(currentQuestionIndex.value, (val) => {
+//   questionStatus.value = `${val + 1} / ${quiz.questions.length}`;
+// });
+const questionStatus = computed(() => {
+  return `${currentQuestionIndex.value} / ${quiz.questions.length}`;
+});
+const barPercentage = computed(
+  () => ` ${(100 * currentQuestionIndex.value) / quiz.questions.length}%`
+);
+
+const onOptionSelected = (isCorrect) => {
+  if (isCorrect) {
+    numberOfCorrectAnswers.value++;
+  }
+
+  if (quiz.questions.length - 1 === currentQuestionIndex.value) {
+    showResults.value = true;
+  }
+
+  currentQuestionIndex.value++;
+};
 </script>
 
 <template>
   <div>
-    <QuestionHeader />
+    <QuestionHeader :questionStatus="questionStatus" :barPercentage="barPercentage" />
     <div>
-      <Question />
+      <Question
+        v-if="!showResults"
+        :question="quiz.questions[currentQuestionIndex]"
+        @selectOption="onOptionSelected"
+      />
+
+      <Result
+        v-else
+        :quizQuestionLength="quiz.questions.length"
+        :numberOfCorrectAnswers="numberOfCorrectAnswers"
+      />
     </div>
   </div>
 </template>
