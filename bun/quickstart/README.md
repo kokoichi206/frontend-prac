e78e4235d3b17ad00a8427bdb390d5c57a6eaf9b
kokoichi206 <tt.300607@gmail.com>
2024-08-24 23:53:23 +0900

    bun quickstart

---
+++ bun/quickstart/README.md
@@ -0,0 +1,10 @@
+[Quickstart](https://bun.sh/docs/quickstart)
+
+``` sh
+bun init
+
+bun run index.ts
+
+
+bun index.ts
+```
---
+++ bun/quickstart/index.ts
@@ -0,0 +1,13 @@
+import figlet from "figlet";
+
+console.log("Hello via Bun!");
+
+const server = Bun.serve({
+  port: 3331,
+  fetch(req) {
+    const body = figlet.textSync("Bun!");
+    return new Response(body);
+  },
+});
+
+console.log(`Listening on http://localhost:${server.port} ...`);
