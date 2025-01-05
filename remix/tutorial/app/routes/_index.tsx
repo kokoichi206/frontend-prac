8e1a2b139e5f741c2b262b95ec43ed6f448fcd24
TAKAHiRO TOMiNAGA <52474650+kokoichi206@users.noreply.github.com>
2025-01-06 00:48:54 +0900

    chore: add login style

--- remix/tutorial/app/routes/_index.tsx
+++ remix/tutorial/app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Form } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,5 +9,64 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-  return <div></div>;
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gradient-to-br from-blue-500 to-purple-600">
+      <div className="w-full max-w-md shadow-md rounded-lg bg-white p-6">
+        <h1 className="text-2xl text-gray-800 text-center font-bold">Login</h1>
+        <Form method="post" className="space-y-6 mt-6">
+          <div>
+            <label
+              htmlFor="name"
+              className="block text-gray-700 font-medium text-sm"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              className="mt-2 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+            />
+          </div>
+
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-gray-700 font-medium text-sm"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="mt-2 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+            />
+          </div>
+
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-gray-700 font-medium text-sm"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              className="mt-2 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg"
+          >
+            Login
+          </button>
+        </Form>
+      </div>
+    </div>
+  );
 }
